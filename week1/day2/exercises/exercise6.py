e6f1cda13c0cd6c75e5e8228e2cfbdd2fdb994b3
lahcenidali6 <lehcenidali6@gmail.com>
2025-05-07 16:23:02 +0100

    solving exercise6

---
+++ week1/day2/exercises/exercise6.py
@@ -0,0 +1,25 @@
+#q1
+#def make_shirt(size, text):
+#    print (f"the size of the shirt is {size} and the text is {text}")
+
+#q2
+#make_shirt("M", "hello world!")
+
+#q3
+#def make_shirt(size="large", text="i love python"):
+#    print (f"the size of the shirt is {size} and the text is {text}")
+
+#q4
+#make_shirt()
+
+#q5
+#def make_shirt(size, text="i love python"):
+#    print (f"the size of the shirt is {size} and the text is {text}")
+#make_shirt(size="medium")
+
+#q6
+def make_shirt(size, text):
+    print (f"the size of the shirt is {size} and the text is {text}")
+make_shirt(size="medium",text="i love python")
+
+
