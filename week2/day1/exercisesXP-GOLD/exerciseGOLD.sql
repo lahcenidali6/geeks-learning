da6d8da90d0d29bbf59eae65aaf60b8965eb0dec
lahcenidali6 <lehcenidali6@gmail.com>
2025-05-12 16:53:40 +0100

    solving exercise gold

---
+++ week2/day1/exercisesXP-GOLD/exerciseGOLD.sql
@@ -0,0 +1,10 @@
+
+select first_name,last_name, birth_date from student order by last_name limit 4
+select first_name,last_name, birth_date from student order by birth_date desc limit 1
+select first_name,last_name, birth_date from student limit 3 offset 2
+
+
+
+
+
+
