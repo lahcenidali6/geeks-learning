ae1ac6bfc954eb3070743b1cb3a16d54cf8c680e
lahcenidali6 <lehcenidali6@gmail.com>
2025-05-13 11:07:15 +0100

    edit exercise 4

--- week1/day3/exercises/exercise4.py
+++ week1/day3/exercises/exercise4.py
@@ -12,25 +12,48 @@ class Zoo:
             print(animal)
     
     def sell_animal(self,animal_sold):
-        self.animals.clear()
-        self.animals.append(animal_sold)
+        if isinstance(self.animals, list):
+            if animal_sold in self.animals:
+                self.animals.remove(animal_sold)
+                print("The animal has been removed :)")
+
+            else :
+               print("the animal isn't in the list!")
+        elif isinstance(self.animals, dict):
+            if animal_sold in self.animals[animal_sold[0].upper()]:
+                self.animals[animal_sold[0].upper()].remove(animal_sold)
+                print("The animal has been removed :)")
+            else :
+                 print("the animal isn't in the list!")
+        
+           
     
     def sort_animals(self):
         list_animals_grouped = {}
         self.animals.sort()
         for animal in self.animals:
-            list_animals_grouped[animal[0]].append(animal)
+            if animal[0].upper() in list_animals_grouped:
+                list_animals_grouped[animal[0]].append(animal)
+            else :
+                list_animals_grouped[animal[0]]=[animal]
         self.animals.clear()
         self.animals=list_animals_grouped
+
     def get_groups(self):
         for group in self.animals:
-            print(f"the group {group} :")
-            for animal in group :
+            print(f"{group} :")
+            for animal in self.animals[group] :
                 print(animal)
+
+
 new_york_zoo=Zoo("new_york_zoo")
-new_york_zoo.add_animal("animal1")
-new_york_zoo.get_animals()
-new_york_zoo.sell_animal("animal1")
+new_york_zoo.add_animal("Ape")
+new_york_zoo.add_animal("Baboon")
+new_york_zoo.add_animal("Eel")
+new_york_zoo.add_animal("Emu")
+new_york_zoo.add_animal("Cat")
+new_york_zoo.sell_animal("Ape")
 new_york_zoo.sort_animals()
+new_york_zoo.get_groups()
 
 
