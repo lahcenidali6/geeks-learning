52c1fdce04d68e9196f490565989cf4762fb7b92
lahcenidali6 <lehcenidali6@gmail.com>
2025-05-13 17:19:07 +0100

    edit exercise

--- week1/day4/exercises/exercise4.py
+++ week1/day4/exercises/exercise4.py
@@ -1,15 +1,39 @@
 class Family:
     def __init__(self , last_name):
-        self.memebers=[{}]
+        self.memebers=[]
         self.last_name=str(last_name)
-    def born(self,**childs):
-        for child in childs:
-            if child not in self.memebers:
-                self.memebers.append(child)
-                print("welcome in our family")
-            else:
-               print("the child is already memebers in family")
+    def born(self,**kwargs):
+        if kwargs["age"]>18:
+            kwargs["is_child"]=False
+        else :
+            kwargs["is_child"]=True
+        if kwargs not in self.memebers:
+            self.memebers.append(kwargs)
+            print("welcome on the family")
+        else:
+            print("is already in the family ")
+    def is_18(self,memeber_name):
+        for memeber in self.memebers:
+            if memeber_name==memeber["name"]:
+                if memeber["age"]>=18:
+                    return True
+                else :
+                    return False
+            else :
+                print("the memebers isn't exist in family")
 
-child1=Family("child")
-child1.born(["youssef","mohammed"])
+    def family_presentation(self):
+        for memeber in self.memebers:
+            print(f"name : {memeber["name"]} , age : {memeber["age"]} , gender : {memeber["gender"]} , is_child : {memeber["is_child"]}")
+        
+
+
+family=Family("family1")
+family.born(name='amin',age=24,gender="male")
+family.born(name='mohammed',age=15,gender="male")
+family.born(name='imane',age=18,gender="female")
+print(family.is_18("amin"))
+family.family_presentation()
+
+
 
