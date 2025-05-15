be880e4c6c4c000fda51f02ef2fa6082f10dbc35
lahcenidali6 <125501569+lahcenidali6@users.noreply.github.com>
2025-05-15 15:30:59 +0200

    h

--- week2/day2/exerciseXP/exerciseXP.sql
+++ week2/day2/exerciseXP/exerciseXP.sql
@@ -7,7 +7,7 @@ select film_id,title , description, release_year, rental_rate from film order by
 select address , phone from address 
 where district = 'Texas';
 
-select * from film where film_id in (15,150);
+select * from film where film_id =15 or film_id =150 ;
 
 select film_id,title , description, length, rental_rate from film where  lower(title) = lower('interstellar');
 
