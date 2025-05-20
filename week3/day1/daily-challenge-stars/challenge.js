d495ab3c5d20b2b09ced16ff088c4bc0e6687bcf
Lahcen Idali <lehcenidali6@gmail.com>
2025-05-20 17:21:00 +0100

    stars challenge

---
+++ week3/day1/daily-challenge-stars/challenge.js
@@ -0,0 +1,12 @@
+for (var i=1;i<7;i++){
+    console.log("*".repeat(i))
+}
+
+for (var i=1;i<7;i++){
+    stars=""
+    for (var j=1;j<i;j++){
+        stars+="*"
+    }
+    console.log(stars)
+    
+}
