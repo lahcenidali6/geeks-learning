f3719ca9bbf6a6b20421eb54fe8f548429d1ebfe
Lahcen Idali <lehcenidali6@gmail.com>
2025-05-23 15:38:46 +0100

    colors game

---
+++ week3/day5/coloring-game-project/coloring-game.js
@@ -0,0 +1,39 @@
+let color = null;
+let mousedown = false;
+
+let body = document.getElementsByTagName("body")[0];
+let color_blocks = document.querySelectorAll("#colors-bar > *");
+let squares = document.querySelectorAll("#squares > *");
+let clear_button = document.getElementsByTagName("button")[0];
+let choice=document.getElementById('choice')
+
+clear_button.addEventListener("click", function(){
+    for (square of squares){
+        square.style.backgroundColor = "white";
+    }
+});
+
+body.addEventListener("mousedown", function(){
+    mousedown = true;
+})
+
+body.addEventListener("mouseup", function(){
+    mousedown = false;
+})
+
+
+for (color_block of color_blocks){
+    color_block.addEventListener("click", function(event){
+        color = event.target.style.backgroundColor;
+        choice.style.backgroundColor=color;
+    });
+}
+
+for (square of squares){
+    square.addEventListener("mousedown", function(event){
+        if (color != null) event.target.style.backgroundColor = color;
+    });
+    square.addEventListener("mouseover", function(event){
+        if (mousedown && color != null) event.target.style.backgroundColor = color;
+    });
+}
